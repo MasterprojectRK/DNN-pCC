7018e13019449335aade01bedcced0bda6ec04c7
Ralf <none@nix.gibts>
2020-09-15 00:33:33 +0200

    loading model and doing simple validation

--- prediction.py
+++ prediction.py
@@ -1,4 +1,8 @@
+from utils import showMatrix
 import click
+import keras
+import numpy as np
+
 
 @click.option("--validationmatrix","-vm", required=False,
                     type=click.Path(exists=True,dir_okay=False,readable=True),
@@ -9,16 +13,40 @@ import click
 @click.option("--outputPath", "-o", required=True,
                     type=click.Path(exists=True,file_okay=False,writable=True),
                     help="Output path where results will be stored")
+@click.option("--trainedmodel", "-trm", required=True,
+                    type=click.Path(exists=True, dir_okay=False),
+                    help="Path+Filename of trained model")
 @click.command()
-def prediction(validationmatrix, chromatinpath, outputpath):
+def prediction(validationmatrix, chromatinpath, outputpath, trainedmodel):
     #check inputs
+    try:
+        trainedModel = keras.models.load_model(trainedmodel)
+    except Exception as e:
+        print(e)
+        msg = "Could not load trained model {:s}. Wrong file?"
+        msg = msg.format(trainedmodel)
+        print(msg)
+
+    #constants for testing
+    windowSize_bins = 80
+
+    #feed trained model with random data to check function
+    np.random.seed(111)
+    input_test = np.random.rand(1, 3, 240, 1)
+    target_test = np.random.rand(1, 3240)
+    loss = trainedModel.evaluate(x=input_test, y=target_test)
+    print("loss: {:.3f}".format(loss))
+    pred = trainedModel.predict(x=input_test)
+    predMatrix = np.zeros((windowSize_bins,windowSize_bins))
+    print(pred[0])
+    predMatrix[np.triu_indices(windowSize_bins)] = pred[0]
+    showMatrix(predMatrix)
 
     #feed inputs through neural network
 
     #compute statistics, if validation matrix provided
 
     #store results
-    pass
 
 if __name__ == "__main__":
     prediction() #pylint: disable=no-value-for-parameter
