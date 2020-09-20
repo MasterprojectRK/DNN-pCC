c1fce612945f7acbdb60ac12e4f2020c4b889382
Ralf <none@nix.gibts>
2020-09-20 17:38:27 +0200

    first really working version

--- prediction.py
+++ prediction.py
@@ -1,4 +1,4 @@
-from utils import showMatrix
+from utils import showMatrix,getMatrixFromCooler,composeChromatinFactors,buildMatrixArray,getBigwigFileList,rebuildMatrix,writeCooler
 import click
 import keras
 import numpy as np
@@ -16,35 +16,87 @@ import numpy as np
 @click.option("--trainedmodel", "-trm", required=True,
                     type=click.Path(exists=True, dir_okay=False),
                     help="Path+Filename of trained model")
+@click.option("--chromosome", "-chrom", required=True,
+                    type=str, default="17", help="chromosome to predict")
 @click.command()
-def prediction(validationmatrix, chromatinpath, outputpath, trainedmodel):
-    #check inputs
+def prediction(validationmatrix, 
+                chromatinpath, 
+                outputpath, 
+                trainedmodel,
+                chromosome):
+    
+    #load the trained model first, since it contains parameters
+    #which must be matched by the remaining inputs
     try:
         trainedModel = keras.models.load_model(trainedmodel)
     except Exception as e:
         print(e)
         msg = "Could not load trained model {:s}. Wrong file?"
         msg = msg.format(trainedmodel)
+        raise SystemExit(msg)
+
+    #derive window size    
+    inputlength = trainedModel.layers[0].input_shape[2]
+    if inputlength % 3 != 0:
+        msg = "Error. Expected input length is {:d} and does not divide by 3"
+        msg = msg.format(inputlength)
+        raise SystemExit(msg)
+    windowsize = int(inputlength / 3)
+    
+    #for testing, provide these numbers as constants
+    binSizeInt = 25000
+    chromLength_bins = 3248
+
+    #load chromatin files first, since it is faster than loading/composing
+    #the validation matrices and if there are too few or too much, 
+    #we can already stop here without computing the matrices.
+    bigwigFileList = getBigwigFileList(chromatinpath)
+    nr_chromatinFactors = trainedModel.layers[0].input_shape[1]
+    if len(bigwigFileList) != nr_chromatinFactors:
+        msg = "Aborting.\n"
+        msg += "Did not find the required number of bigwig files in {:s}\n"
+        msg += "Required: {:d}, Found: {:d}"
+        msg = msg.format(chromatinpath, nr_chromatinFactors, len(bigwigFileList))
+        raise SystemExit(msg)
+    msg = "Found {:d} chromatin factors in {:s}."
+    msg = msg.format(len(bigwigFileList),chromatinpath)
+    print(msg)
+    for factor in bigwigFileList:
+        print(factor)
+
+    #now load relevant part of Hi-C matrix, if provided,
+    #since the bin size will be taken from there
+    if validationmatrix is not None:
+        sparseHiCMatrix, binSizeInt  = getMatrixFromCooler(validationmatrix,chromosome)
+        if sparseHiCMatrix is None:
+            msg = "Could not read HiC matrix {:s} for training, check inputs"
+            msg = msg.format(validationmatrix)
+            raise SystemExit(msg)
+        msg = "Cooler matrix {:s} loaded.\nBin size (resolution) is {:d}bp."
+        msg = msg.format(validationmatrix, binSizeInt)
         print(msg)
+        print("matrix shape", sparseHiCMatrix.shape)
 
-    #constants for testing
-    windowSize_bins = 80
+    #compose chromatin factors
+    chromatinFactorArray = composeChromatinFactors(bigwigFileList,
+                                                           pChromLength_bins=chromLength_bins, 
+                                                           pBinSizeInt=binSizeInt,
+                                                           pChromosomeStr=chromosome,
+                                                           pWindowSize_bins=windowsize)
 
-    #feed trained model with random data to check function
-    np.random.seed(111)
-    input_test = np.random.rand(1, 3, 240, 1)
-    target_test = np.random.rand(1, 3240)
-    loss = trainedModel.evaluate(x=input_test, y=target_test)
-    print("loss: {:.3f}".format(loss))
-    pred = trainedModel.predict(x=input_test)
-    predMatrix = np.zeros((windowSize_bins,windowSize_bins))
-    print(pred[0])
-    predMatrix[np.triu_indices(windowSize_bins)] = pred[0]
-    showMatrix(predMatrix)
+      
+    #feed the chromatin factors through the trained model
+    predMatrixArray = trainedModel.predict(x=chromatinFactorArray)
 
-    #feed inputs through neural network
+    #rebuild the cooler matrix from the predictions and write out
+    meanMatrix = rebuildMatrix(predMatrixArray,windowsize)
+    coolerMatrixName = outputpath + "predMatrix.cool"
+    writeCooler(meanMatrix,binSizeInt,coolerMatrixName,chromosome)
 
-    #compute statistics, if validation matrix provided
+    if validationmatrix is not None:
+        matrixArray = buildMatrixArray(sparseHiCMatrix, windowsize)
+        loss = trainedModel.evaluate(x=chromatinFactorArray, y=matrixArray)
+        print("loss: {:.3f}".format(loss))
 
     #store results
 
