8aaf442207570cac4ef3d09026dbb39ad83ef799
Ralf Krauth <noname@forget.it>
2020-11-01 01:18:16 +0100

    improved help texts

--- training.py
+++ training.py
@@ -16,38 +16,38 @@ tf.random.set_seed(35)
 @click.option("--trainMatrices","-tm",required=True,
                     multiple=True,
                     type=click.Path(exists=True,dir_okay=False,readable=True),
-                    help="Training matrix in cooler format")
+                    help="Training matrices in cooler format (-tm can be specfied multiple times)")
 @click.option("--trainChromatinPaths","-tcp", required=True,
                     multiple=True,
                     type=click.Path(exists=True,readable=True,file_okay=False),
-                    help="Path where chromatin factor data in bigwig format resides (training)")
+                    help="Path where chromatin factor data in bigwig format resides (training, -tcp can be specified multiple times)")
 @click.option("--trainChromosomes", "-tchroms", required=True,
-              type=str, help="chromosome(s) to train on; separate multiple chromosomes by spaces")
+              type=str, help="chromosome(s) to train on; separate multiple chromosomes by spaces, eg. '10 11 12'")
 @click.option("--validationMatrices", "-vm", required=True,
                     multiple=True,
                     type=click.Path(exists=True,dir_okay=False, readable=True),
-                    help="Validation matrix in cooler format")
+                    help="Validation matrices in cooler format, -vm can be specified multiple times")
 @click.option("--validationChromatinPaths","-vcp", required=True,
                     multiple=True,
                     type=click.Path(exists=True,file_okay=False,readable=True),
-                    help="Path where chromatin factor data in bigwig format resides (validation)")
+                    help="Path where chromatin factor data in bigwig format resides (validation, -vcp can be specified multiple times)")
 @click.option("--validationChromosomes", "-vchroms", required=True,
-                    type=str, help="chromosomes for validation; separate multiple chromosomes by spaces")
+                    type=str, help="chromosomes for validation; separate multiple chromosomes by spaces, eg. '10 11 12'")
 @click.option("--sequenceFile", "-sf", required=False,
                     type=click.Path(exists=True,readable=True,dir_okay=False),
                     default=None,
-                    help="Path to DNA sequence in fasta format")
+                    help="Path to DNA sequence in fasta format. If specified, must contain sequences for all training- and validation chromosomes; seq. lengths must match with matrices and bigwig files")
 @click.option("--outputPath", "-o", required=True,
                     type=click.Path(exists=True,file_okay=False,writable=True),
-                    help="Output path where trained network will be stored")
+                    help="Output path where trained network, intermediate data and figures will be stored")
 @click.option("--modelfilepath", "-mfp", required=True, 
               type=click.Path(writable=True,dir_okay=False), 
               default="trainedModel.h5", show_default=True, 
-              help="path+filename for trained model")
+              help="path+filename for trained model in h5 format")
 @click.option("--learningRate", "-lr", required=True,
                 type=click.FloatRange(min=1e-10), 
                 default=1e-5, show_default=True,
-                help="learning rate for stochastic gradient descent")
+                help="learning rate for optimizer")
 @click.option("--numberEpochs", "-ep", required=True,
                 type=click.IntRange(min=2), 
                 default=1000, show_default=True,
@@ -59,42 +59,45 @@ tf.random.set_seed(35)
 @click.option("--recordsize", "-rs", required=False,
                 type=click.IntRange(min=1000), 
                 default=2000, show_default=True,
-                help="size for training/validation data records")
+                help="size (in samples) for training/validation data records")
 @click.option("--windowsize", "-ws", required=True,
                 type=click.IntRange(min=10), 
                 default=80, show_default=True,
                 help="Window size (in bins) for composing training data")
-@click.option("--scaleMatrix", "-scm", required=True,
+@click.option("--scaleMatrix", "-scm", required=False,
                 type=bool, 
                 default=False, show_default=True,
-                help="Scale Hi-C matrix to [0...1]. Default: False")
+                help="Scale Hi-C matrix to [0...1].")
 @click.option("--clampFactors","-cfac", required=False,
                 type=bool, 
                 default=False, show_default=True,
-                help="Clamp outliers in chromatin factor data. Default: False")
+                help="Clamp outliers in chromatin factor data.")
 @click.option("--scaleFactors","-scf", required=False,
                 type=bool, default=True,
                 help="Scale chromatin factor data to range 0...1 (recommended)")
 @click.option("--modelType", "-mod", required=False,
                 type=click.Choice(["initial", "wider", "longer", "wider-longer", "sequence"]),
                 default="initial", show_default=True,
-                help="Type of model to use. Default: wider-longer")
+                help="Type of model to use")
 @click.option("--optimizer", "-opt", required=False,
                 type=click.Choice(["SGD", "Adam", "RMSprop"]),
                 default="SGD", show_default=True,
-                help="Optimizer to use: SGD (default), Adam, RMSprop or cosine similarity.")
+                help="Optimizer to use: SGD, Adam, RMSprop or cosine similarity.")
 @click.option("--loss", "-l", required=False,
                 type=click.Choice(["MSE", "MAE", "MAPE", "MSLE", "Cosine"]),
                 default="MSE", show_default=True,
-                help="Loss function to use, Mean Squared-, Mean Absolute-, Mean Absolute Percentage-, Mean Squared Logarithmic Error or Cosine similarity. Default: MSE")
+                help="Loss function to use, Mean Squared-, Mean Absolute-, Mean Absolute Percentage-, Mean Squared Logarithmic Error or Cosine similarity.")
 @click.option("--earlyStopping", "-early",
                 required=False, type=click.IntRange(min=5),
                 help="patience for early stopping, stop after this number of epochs w/o improvement in validation loss")
 @click.option("--debugState", "-dbs", 
                 required=False, type=click.Choice(["0","10","100","1000","Figures"]),
                 help="debug state for internal use during development")
-@click.option("--figureType", type=click.Choice(["png","svg","pdf"]),
-                required=False, 
+@click.option("--figureType", "-ft",
+                required=False,
+                type=click.Choice(["png","svg","pdf"]),
+                default="png", show_default=True,
+                help="Figure format for plots")
 @click.option("--saveFreq", "-sfreq",
                 required=False,type=click.IntRange(min=1, max=1000),
                 default=50, show_default=True,
