0c68e69d6843e9d9631d3553ff785a5da4ce9f88
Ralf Krauth <noname@forget.it>
2020-11-10 19:34:34 +0100

    improved version working for all batch sizes

--- testScoreLoss.py
+++ testScoreLoss.py
@@ -2,7 +2,7 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Input, Activation
-from testScoreModel import CustomReshapeLayer, DiamondLayer, buildMatrixBranch, makeTrainTestSamples
+from testScoreModel import CustomReshapeLayer, TadInsulationScoreLayer, buildMatrixBranch, makeTrainTestSamples
 
 np.random.seed(35)
 
@@ -11,45 +11,51 @@ def main_fn():
     matsize = 10
     diamondsize = 2
     nr_factors = 6
-    nr_batches = 10
+    nr_batches = 100
+    batchsize = 15
     learningrate = 1e-5
     nr_epochs = 10
     modelPlotName = "scoreLoss.png"
 
+    #generate random training data
     train_features, targ_matrices, targ_scores = makeTrainTestSamples(nr_batches=nr_batches, matsize=matsize, nr_factors=nr_factors, diamondsize=diamondsize)
 
-    model = buildModel(matsize,nr_factors, nr_batches)
+    #generate the model
+    #there is a dummy output so that loss on matrices and scores can be computed
+    model = buildModel(pMatsize=matsize, pNrFactors=nr_factors)
     model.summary()
     tf.keras.utils.plot_model(model,show_shapes=True, to_file=modelPlotName)  
-
-    opt = tf.keras.optimizers.Adam(learning_rate=learningrate, decay=learningrate/nr_epochs)
+    #define the two different loss functions
+    #custom loss function computes TAD insulation score from predicted matrices
     losses = {
         "matrix_out": tf.keras.losses.MeanSquaredError(),
-        "score_out": customLossWrapper(pMatrixsize=matsize, pDiamondsize=diamondsize, pBatchsize=nr_batches)
+        "score_out": customLossWrapper(pMatrixsize=matsize, pDiamondsize=diamondsize)
     }
     lossWeights = {"matrix_out": 1.0, "score_out": 1.0}
+
+    opt = tf.keras.optimizers.Adam(learning_rate=learningrate, decay=learningrate/nr_epochs)
     model.compile(optimizer=opt, loss=losses, loss_weights=lossWeights)
 
     model.fit(x=train_features, 
             y={"matrix_out": targ_matrices, "score_out": targ_scores},
             epochs=nr_epochs,
             verbose=1,
+            batch_size=batchsize
     )
 
-
-def buildModel(pMatsize, pNrFactors, pBatchsize):
+def buildModel(pMatsize, pNrFactors):
     inputs = Input(shape=(pMatsize,pNrFactors))
     matBranch = buildMatrixBranch(inputs, pMatsize, pNrFactors)
     dummyBranch = Activation("linear", name="score_out")(matBranch) #pass-through
     model = Model(inputs=inputs, outputs=[matBranch, dummyBranch], name="testScoreLoss")
     return model
 
-def customLossWrapper(pMatrixsize, pDiamondsize, pBatchsize):
+def customLossWrapper(pMatrixsize, pDiamondsize):
     def customLoss(y_true, y_pred):
         #compute the score from the predicted (flattened) upper triangular matrix
-        predScore = CustomReshapeLayer(pMatrixsize,pBatchsize)(y_pred)
-        predScore = DiamondLayer(pMatrixsize,pDiamondsize,pBatchsize)(predScore)
-        #compute mean squared error
+        predScore = CustomReshapeLayer(matsize=pMatrixsize)(y_pred)
+        predScore = TadInsulationScoreLayer(matsize=pMatrixsize,diamondsize=pDiamondsize)(predScore)
+        #compute mean squared error for TAD insulation score
         predLoss = tf.square(y_true - predScore)
         predLoss = tf.reduce_mean(predLoss)
         return predLoss
--- testScoreModel.py
+++ testScoreModel.py
@@ -10,16 +10,17 @@ def main_fn():
     matsize = 10
     diamondsize = 2
     nr_factors = 6
-    nr_batches = 10
+    nr_batches = 100
+    batchsize = 15
     learningrate = 1e-5
     nr_epochs = 10
     modelPlotName = "scoreModel.png"
 
+    #artificial random training data
     train_features, targ_matrices, targ_scores = makeTrainTestSamples(nr_batches=nr_batches, matsize=matsize, nr_factors=nr_factors, diamondsize=diamondsize)
 
-
     #build the model
-    mod = buildModel(matsize, diamondsize, nr_factors, nr_batches)
+    mod = buildModel(pMatsize=matsize, pDiamondsize=diamondsize, pNrFactors=nr_factors)
     mod.summary()
     tf.keras.utils.plot_model(mod,show_shapes=True, to_file=modelPlotName)
     #compile the model
@@ -31,24 +32,21 @@ def main_fn():
     opt = tf.keras.optimizers.Adam(learning_rate=learningrate, decay=learningrate/nr_epochs)
     mod.compile(optimizer=opt, loss=losses, loss_weights=lossWeights)
 
-
     mod.fit(x=train_features, 
             y={"matrix_out": targ_matrices, "score_out": targ_scores},
             epochs=nr_epochs,
             verbose=1,
+            batch_size=batchsize
     )
 
 
-
-
-
-def buildModel(pMatsize, pDiamondsize, pNrFactors, pBatchsize):
+def buildModel(pMatsize, pDiamondsize, pNrFactors):
     inputs = Input(shape=(pMatsize,pNrFactors))
-    matBranch = buildMatrixBranch(inputs, pMatsize, pNrFactors) 
-    scoreBranch = buildScoreBranch(matBranch, pMatsize, pDiamondsize, pBatchsize)
-    #fullModel = Model(inputs=matModel.inputs, outputs=[matModel.outputs,scoreModel.outputs])
+    matBranch = buildMatrixBranch(pInputs=inputs, pMatsize=pMatsize, pNrFactors=pNrFactors) 
+    scoreBranch = buildScoreBranch(pInputs=matBranch, pMatsize=pMatsize, pDiamondsize=pDiamondsize)
     model = Model(inputs=inputs, outputs=[matBranch,scoreBranch], name="testScoreModel")
     return model
+
 
 def buildMatrixBranch(pInputs,pMatsize, pNrFactors):
     out_neurons = int( pMatsize * (pMatsize + 1) / 2 )
@@ -59,10 +57,12 @@ def buildMatrixBranch(pInputs,pMatsize, pNrFactors):
     x = Dense(out_neurons, activation="relu", name="matrix_out")(x)
     return x
 
-def buildScoreBranch(pInputs, pMatsize, pDiamondsize, pBatchsize):
-    x = CustomReshapeLayer(pMatsize, pBatchsize)(pInputs)
-    x = DiamondLayer(pMatsize,pDiamondsize,pBatchsize, name="score_out")(x)
+
+def buildScoreBranch(pInputs, pMatsize, pDiamondsize):
+    x = CustomReshapeLayer(matsize=pMatsize)(pInputs)
+    x = TadInsulationScoreLayer(matsize=pMatsize, diamondsize=pDiamondsize, name="score_out")(x)
     return x
+
 
 def makeTrainTestSamples(nr_batches, matsize, nr_factors, diamondsize):
     #train features
@@ -86,69 +86,57 @@ def makeTrainTestSamples(nr_batches, matsize, nr_factors, diamondsize):
 
 
 class CustomReshapeLayer(tf.keras.layers.Layer):
-    def __init__(self, matsize, batchsize):
-        super(CustomReshapeLayer, self).__init__()
+    '''
+    reshape a 1D tensor such that it represents 
+    the upper triangular part of a square 2D matrix with shape (matsize, matsize)
+    #example: 
+     [1,2,3,4,5,6] => [[1,2,3],
+                       [0,4,5],
+                       [0,0,6]]
+    '''
+    def __init__(self, matsize, **kwargs):
+        super(CustomReshapeLayer, self).__init__(**kwargs)
         self.matsize = matsize
-        self.tensList = []
-        #self.batchsize = int(batchsize)
         self.triu_indices = [ [x,y] for x,y in zip(np.triu_indices(self.matsize)[0], np.triu_indices(self.matsize)[1]) ]
+
+    def call(self, inputs):      
+        return tf.map_fn(self.pickItems, inputs)
     
-    def build(self, input_shape):
-        print("ins", input_shape)
-        self.batchsize = input_shape[0]
-
-    @tf.function
-    def call(self, inputs):
-        #for i in range(self.batchsize):
-        if self.batchsize is not None:
-            for i in range(self.batchsize):
-                try: 
-                    self.tensList.append(self.pickItems(i, inputs))
-                except:
-                    #for batches that do not have the full number of samples
-                    #e.g. last batch in a dataset
-                    #no idea how to do this properly
-                    pass
-        return tf.stack(self.tensList)
-
-    def pickItems(self, batch_index, inputs):
-        #pick the right items from flattened upper triangular matrix part
-        #and stack them such that a (matrix_size, matrix_size) shaped tensor is created
+    def pickItems(self, inputVec):
         sparseTriuTens = tf.SparseTensor(self.triu_indices, 
-                                        values=inputs[batch_index], 
+                                        values=inputVec, 
                                         dense_shape=[self.matsize, self.matsize] )
         return tf.sparse.to_dense(sparseTriuTens)
 
-class DiamondLayer(tf.keras.layers.Layer):
-    def __init__(self, matsize, diamondsize, batchsize, **kwargs):
-        super(DiamondLayer, self).__init__(**kwargs)
+class TadInsulationScoreLayer(tf.keras.layers.Layer):
+    '''
+    Computes TAD insulation scores for square 2D tensors with shape (matsize,matsize)
+    and fixed-size insulation blocks ("diamonds") with shape (diamondsize,diamondsize)
+    '''
+    def __init__(self, matsize, diamondsize, **kwargs):
+        super(TadInsulationScoreLayer, self).__init__(**kwargs)
         self.matsize = int(matsize)
         self.diamondsize = int(diamondsize)
-        self.batchsize = int(batchsize)
-        self.tensList = []
+        if self.diamondsize >= self.matsize:
+            msg = "Diamondsize {:d} must be smaller than matrix size {:d}"
+            msg = msg.format(self.diamondsize, self.matsize)
+            raise ValueError(msg)
     
-    @tf.function
     def call(self, inputs):
-        for i in range(self.batchsize):
-            try:
-                self.tensList.append(self.pickElements(i, inputs))
-            except:
-                #for batches that do not have the full number of samples
-                #e.g. last batch in a dataset
-                #no idea how to do this properly
-                pass
-        return tf.stack(self.tensList)
+        return tf.map_fn(self.pickItems, inputs)
 
-    def pickElements(self, batch_index, inputs):
+    def pickItems(self, inputMat):
         nr_diamonds = self.matsize - 2*self.diamondsize
         start_offset = self.diamondsize
         rowEndList = [i + start_offset for i in range(nr_diamonds)]
         rowStartList = [i-self.diamondsize for i in rowEndList] 
         columnStartList = [i+1 for i in rowEndList]
         columnEndList = [i+self.diamondsize for i in columnStartList]
-        l = [ inputs[batch_index][i:j,k:l] for i,j,k,l in zip(rowStartList,rowEndList,columnStartList,columnEndList) ]
+        l = [ inputMat[i:j,k:l] for i,j,k,l in zip(rowStartList,rowEndList,columnStartList,columnEndList) ]
         l = [ tf.reduce_mean(i) for i in l ]
         return tf.stack(l)
+
+
 
 if __name__ == "__main__":
     main_fn()
